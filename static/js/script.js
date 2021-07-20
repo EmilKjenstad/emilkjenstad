f7a87e6ede5d878e63a71f9971419ab07a50044f
Emil Kjenstad <emk113@pitt.edu>
2021-07-20 18:37:45 +0200

    Major changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 function chbg(img) {
   document.getElementById("thisTest").src = "./static/assets/img/"+img+".png";
 }
+
+function showContent(selector) {
+  document.getElementById(selector).style.display = "inline-block";
+}
